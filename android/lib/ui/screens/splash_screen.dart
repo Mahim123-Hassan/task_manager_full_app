493818556fe2fd0cd85a01b80bb89f0c9580c3b4
Mahim123-Hassan <hmahim951@gmail.com>
2026-03-06 10:34:34 +0600

    first commit

---
+++ android/lib/ui/screens/splash_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../utils/asset_paths.dart';
+import 'sign_in_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _moveToNextScreen();
+  }
+
+  Future<void> _moveToNextScreen() async {
+    await Future.delayed(Duration(seconds: 2));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => SignInScreen()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            AssetPaths.backgroundSvg,
+            height: double.maxFinite,
+            width: double.maxFinite,
+            fit: BoxFit.cover,
+          ),
+          Center(child: Image.asset(AssetPaths.logoPng, width: 220)),
+        ],
+      ),
+    );
+  }
+}
---
+++ android/lib/utils/asset_paths.dart
@@ -0,0 +1,6 @@
+class AssetPaths {
+  static const String _imagesUrl="assets/images/";
+  static const String backgroundSvg='$_imagesUrl/background.svg';
+  static const String logoPng='$_imagesUrl/logo.png';
+
+}
