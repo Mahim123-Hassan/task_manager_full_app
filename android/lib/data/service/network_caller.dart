7fa20eff7d8a513559e88278a7af69ff0b7ad929
Mahim123-Hassan <hmahim951@gmail.com>
2026-03-13 22:58:19 +0600

    added network_caller

--- android/lib/data/service/network_caller.dart
+++ android/lib/data/service/network_caller.dart
@@ -3,36 +3,33 @@ import 'dart:convert';
 import 'package:http/http.dart';
 
 class NetworkCaller {
-  
-  Future<NetworkResponse> getRequest (String url)async{
+  Future<NetworkResponse>getRequest(String url)async{
     Uri uri = Uri.parse(url);
-    Response response =await get(uri);
-    final decodedData=jsonDecode(response.body);
-    
-    if(response.statusCode==200){
-      return NetworkResponse(isSuccess: true, responseCode: response.statusCode,
-          body: decodedData,);
 
+    Response response =await get(uri);
+    final decodedData = jsonDecode(response.body);
+
+    if(response.statusCode==200){
+      return NetworkResponse(isSuccess: true,
+          responseCode: response.statusCode,
+        body: decodedData,
+      );
     }else{
       return NetworkResponse(isSuccess: false,
-          responseCode: response.statusCode, errorMessage: '',
-          );
+          responseCode: response.statusCode);
     }
   }
-
 }
-
-  class NetworkResponse{
+class NetworkResponse {
   final bool isSuccess;
   final int responseCode;
   final dynamic body;
   final String? errorMessage;
 
-
   NetworkResponse({
     required this.isSuccess,
     required this.responseCode,
     this.body,
-     this.errorMessage,
+    this.errorMessage,
   });
-  }
+}
